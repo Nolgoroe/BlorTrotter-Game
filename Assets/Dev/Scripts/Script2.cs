df9b9eba06c2435c8342852f55c21149123c4e57
Avishy <Avishy.menashe@gmail.com>
2022-02-01 16:22:18 +0100

    Another Test

---
+++ Assets/Dev/Scripts/Script2.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script2 : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("SUCCKKKKAAAAA");
+    }
+
+}
